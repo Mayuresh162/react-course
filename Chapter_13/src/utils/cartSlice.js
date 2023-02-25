bde2cc177ec1e28f2875c9c88b3031b5647b568d
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2023-02-25 17:43:19 +0530

    Add files via upload

--- Chapter_13/src/utils/cartSlice.js
+++ Chapter_13/src/utils/cartSlice.js
@@ -8,11 +8,29 @@ const cartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
-            state.items.push(action.payload);
+            const doesItemExist = state.items.find((item) => item.id === action.payload.id);
+            if(doesItemExist){
+                state.items.forEach(item => {
+                    if (item.id === action.payload.id) {
+                        item = { ...item, count: item.count++ }
+                    }
+                })
+            } else {
+                state.items.push({ ...action.payload, count: 1})
+            }
         },
         removeItem: (state, action) => {
-            const filteredItems = state.items.filter(item => item.id !== action.payload);
-            return {...state, items: filteredItems};
+            const doesItemExist = state.items.find((item) => item.id === action.payload);
+            if(doesItemExist && doesItemExist.count > 1) {
+                state.items.forEach(item => {
+                    if (item.id === action.payload) {
+                        item = { ...item, count: item.count-- }
+                    }
+                })
+            } else {
+                const filteredItems = state.items.filter(item => item.id !== action.payload);
+                return {...state, items: filteredItems}; 
+            }
         },
         clearCart: (state) => {
             state.items = [];
